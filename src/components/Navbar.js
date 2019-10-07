75272231f12ae983c855827ccfa3f7d0d2c3675d
Caleb Spindler <caleb.spindler@gmail.com>
2019-10-07 11:27:23 -0700

    defined logo for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-// import logo from '../img/finding-rubies-logo-192.png'
+import logo from '../img/finding-rubies-logo-192.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
