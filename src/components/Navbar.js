a3f8ea10a875692d67a9e59466e8c193fca8283e
Caleb Spindler <caleb.spindler@gmail.com>
2019-10-07 11:53:51 -0700

    added logo styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Finding Rubies" style={{ width: '88px' }} />
+              <img src={logo} className="navbar-logo" alt="Finding Rubies" />
               Finding Rubies
             </Link>
             {/* Hamburger menu */}
